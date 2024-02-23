96e67095dbe1dd6f0197e313f10776f0fec7b824
amdsmi <amdsmi.ca@gmail.com>
2024-02-23 08:38:56 -0500

    # Update config file

--- backend/src/config.py
+++ backend/src/config.py
@@ -2,9 +2,9 @@ import os
 
 APP_SECRET_KEY = os.getenv("APP_SECRET_KEY", 'flask_oauth')
 
-GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID", '253692784240-bnp7l0igljg2jppa1c3gjlk0ko6kaa04.apps.googleusercontent.com')
+GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
 
-GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET", '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')
+GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
 
 FRONT_SERVICE_ADDRESS = os.getenv('FRONT_HOST', 'http://127.0.0.1:3000')
 
